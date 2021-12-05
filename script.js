d24dcc45c911168fe0abb9672267b476d45cea5e
Jan Lochman <jan.lochman@icloud.com>
2021-12-05 20:51:14 +0100

    script update

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 pubKey = new solanaWeb3.PublicKey('4PFAeWd5Fq1TkX2jQeohqjwr4cj3LBoLy64omRPBoUj9');
-
-
+connection = new solanaWeb3.Connection(solanaWeb3.clusterApiUrl('mainnet-beta'));
 
 async function refresh() {
-    let connection = new solanaWeb3.Connection(solanaWeb3.clusterApiUrl('mainnet-beta'));
     let balance = await connection.getBalance(pubKey);
+    balance -= 0.14
     balance *= 1e-9;
 
     let tokensMinted = Math.round(42 + balance / 0.75);
@@ -12,7 +11,7 @@ async function refresh() {
     percentMinted = Math.round(percentMinted * 1000) / 1000
     
     let myDiv = document.getElementById('my-div')
-    myDiv.innerText=`Tokens Minted: ${tokensMinted}/4914 [${percentMinted}%], balance: ${balance} SOL.`
+    myDiv.innerText=`Tokens Minted: ${tokensMinted}/4914 [${percentMinted}%], remains: ${4914-tokensMinted}, balance: ${balance} SOL.`
 }
 
 window.addEventListener('load', refresh());
