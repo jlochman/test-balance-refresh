d853ec4d0bdb14d2635d07cafeddf36c1ff80f94
Jan Lochman <jan.lochman@icloud.com>
2022-02-04 17:10:02 +0100

    HL royalties

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-pubKey = new solanaWeb3.PublicKey('4PFAeWd5Fq1TkX2jQeohqjwr4cj3LBoLy64omRPBoUj9');
+pubKey = new solanaWeb3.PublicKey('GxmVrjJCHvoC3fJfoq6jAckxhauhzs7x25fuXZAUd8Hu');
 connection = new solanaWeb3.Connection(solanaWeb3.clusterApiUrl('mainnet-beta'));
 
 async function refresh() {
     let balance = await connection.getBalance(pubKey);
     balance *= 1e-9;
-    balance -= 0.14
+    balance -= 0.064995;
+    balance += 0.035;
 
-    let tokensMinted = Math.round(42 + balance / 0.75);
-    let percentMinted = 100 * tokensMinted / 4914;
-    percentMinted = Math.round(percentMinted * 1000) / 1000
+    let volume = 20 * balance / 0.27;
     
-    let myDiv = document.getElementById('my-div')
-    myDiv.innerText=`Tokens Minted: ${tokensMinted}/4914 [${percentMinted}%], remains: ${4914-tokensMinted}, balance: ${balance} SOL.`
+    document.getElementById('volume-div').innerText=`Volume: ${volume} SOL`
+    document.getElementById('royalties-div').innerText=`Royalties: ${volume / 20 * 0.9} SOL`
 }
 
 window.addEventListener('load', refresh());
